108ed1f0a57295b8abcb9ff37150aca833bcd665
Collin Morudi <collinmorudi@gmail.com>
2026-05-31 15:53:56 +0200

    Solved project euler 001 problem: multiples of 3 and 5

---
+++ project-euler-solutions/solution.js
@@ -0,0 +1,13 @@
+// Find the sum of all the multiples of 3 or 5 
+// below the provided parameter value number.
+
+function multiplesOf3Or5(number) {
+  let sum = 0;
+
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
